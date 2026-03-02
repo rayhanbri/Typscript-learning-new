fb1f21de88369d52778309c2267900cb91e351c4
lebwebsite02 <lebwebsite02@gmail.com>
2026-03-03 03:43:12 +0600

    cofigaration changes

---
+++ dist/1basic.js
@@ -0,0 +1,3 @@
+"use strict";
+let message = "hello : typscript";
+console.log(message);
---
+++ src/1basic.ts
@@ -0,0 +1,5 @@
+let message:string ="hello : typscript";
+
+console.log(message)
+
+// now run tsc to create dist file
