1d71ffc8b560f3f0b2b26a93a0b5f0b9d0ad3cf4
lebwebsite02 <lebwebsite02@gmail.com>
2026-03-03 01:07:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("hello");
+// tsc Script.ts dile shob dhekhe jabe 
+//create script.ts
+var country = "bangladesh";
+console.log(country);
---
+++ script.ts
@@ -0,0 +1,7 @@
+console.log("hello");
+
+// tsc Script.ts dile shob dhekhe jabe 
+//create script.ts
+
+const country="bangladesh";
+console.log(country)
