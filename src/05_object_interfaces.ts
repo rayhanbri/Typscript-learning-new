fca32ec8940f6f49c43c3a7ec968c28b66ea662a
lebwebsite02 <lebwebsite02@gmail.com>
2026-03-03 20:02:10 +0600

    interface with methods

--- src/05_object_interfaces.ts
+++ src/05_object_interfaces.ts
@@ -16,3 +16,18 @@ let  user : User  = {
 }
 
 // user.id=4 
+
+//Interface with methods   //bujhi nai akhono 
+interface Product {
+  name: string;
+  price: number;
+  getDiscount(percentage: number): number;
+}
+
+let laptop: Product = {
+  name: "MacBook Pro",
+  price: 2000,
+  getDiscount(percentage: number): number {
+    return this.price * (percentage / 100);
+  },
+};
